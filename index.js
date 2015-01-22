fda8bb87ba6479bda3eddee8ace8b37f1a12d11f
Mariusz Nowak <medyk@medikoo.com>
2015-01-22 14:42:38 +0100

    Rename `resolve` to `normalize`

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var callable  = require('es5-ext/object/valid-callable')
   , validate  = require('./lib/validate')
   , getRouter = require('./lib/get-router')
 
-  , defaultResolve = function (result) { return result || true; };
+  , defaultNormalize = function (result) { return result || true; };
 
 module.exports = function (routes/*, options*/) {
 	var options = Object(arguments[1]);
-	if (options.resolve !== undefined) callable(options.resolve);
-	return getRouter(validate(routes), options.resolve || defaultResolve);
+	if (options.normalizeResult !== undefined) callable(options.normalizeResult);
+	return getRouter(validate(routes), options.normalizeResult || defaultNormalize);
 };
