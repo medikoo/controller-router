bcd83d72f3421daf4873325650b322945dccbb87
Mariusz Nowak <medyk@medikoo.com>
2015-06-22 13:44:16 +0200

    Update tests after context exposure

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 var clear = require('es5-ext/array/#/clear');
 
 module.exports = function (t, a) {
-	var called = [], obj = {}, conf;
+	var called = [], obj = {}, conf, context = {};
 	var router = t(conf = {
 		'/': function () { called.push('root'); },
 		foo: function () { called.push('foo'); },
@@ -28,57 +28,61 @@ module.exports = function (t, a) {
 		'elo/dwa/filo': function () { called.push('elo/dwa/filo'); }
 	});
 
-	a.deep(router('/'), { conf: conf['/'], result: undefined });
+	a.deep(router.call(context, '/'), { conf: conf['/'], result: undefined, context: context });
 	a.deep(called, ['root']);
 	clear.call(called);
 
-	a.deep(router('/foo/'), { conf: conf.foo, result: undefined });
+	a.deep(router.call(context, '/foo/'), { conf: conf.foo, result: undefined, context: context });
 	a.deep(called, ['foo']);
 	clear.call(called);
 
-	a(router('miszka'), false);
+	a(router.call(context, 'miszka'), false);
 	a.deep(called, []);
 
-	a.deep(router('marko'), { conf: conf.marko, result: undefined });
+	a.deep(router.call(context, 'marko'), { conf: conf.marko, result: undefined, context: context });
 	a.deep(called, ['marko']);
 	clear.call(called);
 
-	a.deep(router('bar/dwa'), { conf: conf['bar/dwa'], result: obj });
+	a.deep(router.call(context, 'bar/dwa'), { conf: conf['bar/dwa'], result: obj, context: context });
 	a.deep(called, ['bar/dwa']);
 	clear.call(called);
 
-	a.deep(router('elo/dwa'), { conf: conf['elo/dwa'], result: undefined });
+	a.deep(router.call(context, 'elo/dwa'),
+		{ conf: conf['elo/dwa'], result: undefined, context: context });
 	a.deep(called, ['elo/dwa']);
 	clear.call(called);
 
-	a(router('elo/dwa/marko'), false);
+	a(router.call(context, 'elo/dwa/marko'), false);
 	a.deep(called, ['elo/dwa/*:match']);
 	clear.call(called);
 
-	a.deep(router('elo/dwa/foo'), { conf: conf['elo/dwa/[a-z]+'], result: undefined });
+	a.deep(router.call(context, 'elo/dwa/foo'),
+		{ conf: conf['elo/dwa/[a-z]+'], result: undefined, context: context });
 	a.deep(called, ['elo/dwa/*:match', 'elo/dwa/*:controller']);
 	clear.call(called);
 
-	a(router('elo/dwa/foo/foo/ilo'), false);
+	a(router.call(context, 'elo/dwa/foo/foo/ilo'), false);
 	a.deep(called, ['elo/dwa/*/foo/*:match2']);
 	clear.call(called);
 
-	a.deep(router('elo/dwa/foo/foo/bar'),
-		{ conf: conf['elo/dwa/[a-z]+/foo/[a-z]+'], result: undefined });
+	a.deep(router.call(context, 'elo/dwa/foo/foo/bar'),
+		{ conf: conf['elo/dwa/[a-z]+/foo/[a-z]+'], result: undefined, context: context });
 	a.deep(called, ['elo/dwa/*/foo/*:match2', 'elo/dwa/*/foo/*:controller']);
 	clear.call(called);
 
-	a(router('elo/dwa/abla/bar'), false);
+	a(router.call(context, 'elo/dwa/abla/bar'), false);
 	a.deep(called, []);
 
-	a(router('elo/dwa/foo/bar/miko'), false);
+	a(router.call(context, 'elo/dwa/foo/bar/miko'), false);
 	a.deep(called, []);
 
-	a.deep(router('elo/dwa/filo'), { conf: conf['elo/dwa/filo'], result: undefined });
+	a.deep(router.call(context, 'elo/dwa/filo'),
+		{ conf: conf['elo/dwa/filo'], result: undefined, context: context });
 	a.deep(called, ['elo/dwa/filo']);
 	clear.call(called);
 
-	a.deep(router('elo/trzy'), { conf: conf['elo/trzy'], result: undefined });
+	a.deep(router.call(context, 'elo/trzy'),
+		{ conf: conf['elo/trzy'], result: undefined, context: context });
 	a.deep(called, ['elo/trzy']);
 	clear.call(called);
 };
