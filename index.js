1c0082caa591f544126a2eb13ebc0a998fb3c63d
Mariusz Nowak <medyk@medikoo.com>
2017-05-23 15:28:25 +0200

    style: fix lint issue

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ var ControllerRouter = module.exports = Object.defineProperties(function (routes
 	options = Object(arguments[1]);
 	this.constructor.ensureRoutes(routes, options);
 
-	defineProperty(this, 'eventProto', d((options.eventProto != null) ? ensureObject(options.eventProto) : {}));
+	defineProperty(this, 'eventProto',
+		d((options.eventProto != null) ? ensureObject(options.eventProto) : {}));
 
 	defineProperty(this, 'routes', d(routes));
 
